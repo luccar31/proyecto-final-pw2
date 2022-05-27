c0cf8d18cf70c0fe0dd99ae8964e786436987e92
Lucas <lucas.david.car99@gmail.com>
2022-05-27 00:01:37 -0300

    + Se modificó la estructura de la base de datos

--- sql-scripts/database.sql
+++ sql-scripts/database.sql
@@ -8,26 +8,20 @@ CREATE TABLE role(
 );
 
 CREATE TABLE user(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nickname VARCHAR(50) NOT NULL UNIQUE,
+    nickname VARCHAR(50) PRIMARY KEY,
     password VARCHAR(50) NOT NULL,
     role INT NOT NULL,
     FOREIGN KEY (role) REFERENCES role(id)
 );
 
 CREATE TABLE client(
-    id_user INT PRIMARY KEY,
-    FOREIGN KEY (id_user) REFERENCES user(id),
+    user_nickname VARCHAR(50) PRIMARY KEY,
+    FOREIGN KEY (user_nickname) REFERENCES user(nickname),
     firstname VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
-);
-
-CREATE TABLE traveler(
-    id_traveler INT NOT NULL,
-    FOREIGN KEY (id_traveler) REFERENCES client(id_user),
-    traveler_code VARCHAR(50) PRIMARY KEY,
-    flight_level INT NOT NULL
+    email VARCHAR(100) NOT NULL UNIQUE,
+    traveler_code VARCHAR(50),
+    flight_level INT
 );
 
 CREATE TABLE medic_center(
@@ -36,13 +30,27 @@ CREATE TABLE medic_center(
     daily_limit INT NOT NULL
 );
 
+CREATE TABLE appointment(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    user_nickname VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_nickname) REFERENCES client(user_nickname)
+);
+
 INSERT INTO medic_center VALUES(1, 'Buenos Aires', 300);
 INSERT INTO medic_center VALUES(2, 'Shanghái', 210);
 INSERT INTO medic_center VALUES(3, 'Ankara', 200);
 
-CREATE TABLE appointment(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES client(id_user)
-);
+INSERT INTO role VALUES (1, 'client');
+INSERT INTO role VALUES (2, 'admin');
+
+INSERT INTO user VALUES ('lucas1234', '1234', 2);
+INSERT INTO user VALUES ('stefi5678', '1234', 1);
+INSERT INTO user VALUES ('tomi4321', '1234', 1);
+INSERT INTO user VALUES ('maxi9876', '1234', 1);
+INSERT INTO user VALUES ('alexis7777', '1234', 1);
+
+INSERT INTO client VALUES ('stefi5678', 'Stefenía', 'Rinaldi', 'stefania@gmail.com', null, null);
+INSERT INTO client VALUES ('tomi4321', 'Tomás', 'Palavecino', 'tomas@gmail.com', null, null);
+INSERT INTO client VALUES ('maxi9876', 'Maximiliano', 'Davies', 'maxi@gmail.com', null, null);
+INSERT INTO client VALUES ('alexis7777', 'Alexis', 'Verba', 'alexis@gmail.com', null, null);
