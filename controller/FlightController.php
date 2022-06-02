7971e113a6c31c366dee9a0d80fbf42b08f2b7c7
Stefania <rinaldi.stefania09@gmail.com>
2022-06-02 17:01:35 -0300

    Modificacion del execute para que devuelva vuelos, para que se muestren en la pestaña de viajes. Se agrego el metodo searchFlight para buscar los vuelos por tipo

--- controller/FlightController.php
+++ controller/FlightController.php
@@ -5,19 +5,17 @@ class FlightController
     private $printer;
     private $flightModel;
 
-    public function __construct($appointmentModel, $printer)
-    {
-        $this->flightModel = $appointmentModel;
+    public function __construct($flightModel, $printer){
+        $this->flightModel = $flightModel;
         $this->printer = $printer;
     }
 
-    public function execute()
-    {
-        $this->printer->generateView('flightView.html');
+    public function execute(){
+        $flights = $this->flightModel->getAllFlights();
+        $this->printer->generateView('flightView.html', $flights);
     }
 
-    public function getFlights()
-    {
+    public function getFlights(){
         $origin = $_POST['origin'];
         $destination = $_POST['destination'];
         $flightType = $_POST['flightType'];
@@ -25,6 +23,25 @@ class FlightController
         $data = $this->flightModel->getFlights($origin, $destination, $flightType);
 
         $this->printer->generateView('flightView.html', $data);
+
+    }
+
+    public function searchFlight(){
+        $typeFlight = isset($_POST['typeFlight'])? $_POST['typeFlight'] : "";
+        $typeCabin = isset($_POST['$typeCabin'])? $_POST['$typeCabin'] : "";
+        $typeService = isset($_POST['$typeService'])? $_POST['$typeService'] : "";
+
+        if($typeFlight) {
+            $flights = $this->flightModel->search($typeFlight);
+            $this->printer->generateView('flightView.html', $flights);
+        }else{
+            $this->execute();
+        }
+
+        if($typeFlight && $typeCabin){
+            $flights = $this->flightModel->search($typeFlight, $typeCabin);
+            $this->printer->generateView('flightView.html', $flights);
+        }
 
     }
 }
