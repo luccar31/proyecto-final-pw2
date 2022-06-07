6bdc88dc1c639193b4fad732462d0b3356ab5ed6
Stefania <rinaldi.stefania09@gmail.com>
2022-06-07 18:49:20 -0300

    Agregue un metodo para buscar por id de plan de vuelo

--- model/Flight_planModel.php
+++ model/Flight_planModel.php
@@ -17,14 +17,19 @@ class Flight_planModel{
     }
 
     public function search($typeFlight){
-
-        if($typeFlight){
-            $result =  $this->database->query("SELECT fp.id, tf.description, fp.departure_day, fp.departure_time, fp.departure, fp.destination, fp.ship_model
+        $result =  $this->database->query("SELECT fp.id, tf.description, fp.departure_day, fp.departure_time, fp.departure, fp.destination, fp.ship_model
                                                 FROM flight_plan fp
                                                 INNER JOIN type_flight tf ON fp.id_type = tf.id
                                                 WHERE id_type = '$typeFlight'");
             return  ['flight_plans' => $result];
-        }
+    }
+
+    public function searchForId($id_flight_plan){
+        $result =  $this->database->query("SELECT fp.id, tf.description, fp.departure_day, fp.departure_time, fp.departure, fp.destination, fp.ship_model
+                                                FROM flight_plan fp
+                                                INNER JOIN type_flight tf ON fp.id_type = tf.id
+                                                WHERE fp.id = '$id_flight_plan'");
+        return  ['flight_plans' => $result];
 
     }
 
