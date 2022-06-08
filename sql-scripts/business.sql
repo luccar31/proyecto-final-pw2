c2323d821273e4407308408a9f22d42816a0024c
Lucas <lucas.david.car99@gmail.com>
2022-06-08 02:08:34 -0300

    + Se unificó el script sql para facilitar la creación de la base de datos

--- sql-scripts/business.sql
+++ sql-scripts/business.sql
@@ -1,4 +1,79 @@
+CREATE DATABASE gauchorocket;
 USE gauchorocket;
+
+CREATE TABLE role
+(
+    id          INT PRIMARY KEY,
+    description VARCHAR(20)
+);
+
+INSERT INTO role
+VALUES (1, 'client');
+INSERT INTO role
+VALUES (2, 'admin');
+
+CREATE TABLE user
+(
+    nickname VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(50) NOT NULL,
+    role     INT         NOT NULL,
+    FOREIGN KEY (role) REFERENCES role (id)
+);
+
+INSERT INTO user
+VALUES ('lucas1234', '81dc9bdb52d04dc20036dbd8313ed055', 2);
+INSERT INTO user
+VALUES ('stefi5678', '81dc9bdb52d04dc20036dbd8313ed055', 1);
+INSERT INTO user
+VALUES ('tomi4321', '81dc9bdb52d04dc20036dbd8313ed055', 1);
+INSERT INTO user
+VALUES ('maxi9876', '81dc9bdb52d04dc20036dbd8313ed055', 1);
+INSERT INTO user
+VALUES ('alexis7777', '81dc9bdb52d04dc20036dbd8313ed055', 1);
+
+CREATE TABLE client
+(
+    user_nickname VARCHAR(50) PRIMARY KEY,
+    firstname     VARCHAR(50)  NOT NULL,
+    surname       VARCHAR(50)  NOT NULL,
+    email         VARCHAR(100) NOT NULL UNIQUE,
+    traveler_code VARCHAR(10) UNIQUE,
+    flight_level  INT,
+    FOREIGN KEY (user_nickname) REFERENCES user (nickname)
+);
+
+INSERT INTO client
+VALUES ('stefi5678', 'Stefenía', 'Rinaldi', 'stefania@gmail.com', null, null);
+INSERT INTO client
+VALUES ('tomi4321', 'Tomás', 'Palavecino', 'tomas@gmail.com', null, null);
+INSERT INTO client
+VALUES ('maxi9876', 'Maximiliano', 'Davies', 'maxi@gmail.com', null, null);
+INSERT INTO client
+VALUES ('alexis7777', 'Alexis', 'Verba', 'alexis@gmail.com', null, null);
+
+CREATE TABLE medical_center
+(
+    id          INT PRIMARY KEY,
+    name        VARCHAR(30) NOT NULL,
+    daily_limit INT         NOT NULL
+);
+
+INSERT INTO medical_center
+VALUES (1, 'Buenos Aires', 300);
+INSERT INTO medical_center
+VALUES (2, 'Shanghái', 210);
+INSERT INTO medical_center
+VALUES (3, 'Ankara', 200);
+
+CREATE TABLE appointment
+(
+    id                INT AUTO_INCREMENT PRIMARY KEY,
+    date              DATE        NOT NULL,
+    user_nickname     VARCHAR(50) NOT NULL UNIQUE,
+    FOREIGN KEY (user_nickname) REFERENCES client (user_nickname),
+    id_medical_center INT         NOT NULL,
+    FOREIGN KEY (id_medical_center) REFERENCES medical_center (id)
+);
 
 CREATE TABLE type_cabin
 (
@@ -20,7 +95,6 @@ CREATE TABLE cabin
     id_type  INT NOT NULL,
     capacity INT NOT NULL,
     FOREIGN KEY (id_type) REFERENCES type_cabin (id)
-
 );
 
 -- cabinas
